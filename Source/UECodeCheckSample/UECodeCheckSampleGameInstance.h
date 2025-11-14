40338488ec46aef5f6c583eda7ab00b6cb96a35a
Rafal Lazinski <rafal.lazinski@gmail.com>
2025-11-14 15:39:17 +0100

    feat: Add UUECodeCheckSampleGameInstance::WorldWithoutUProp;

--- Source/UECodeCheckSample/UECodeCheckSampleGameInstance.h
+++ Source/UECodeCheckSample/UECodeCheckSampleGameInstance.h
@@ -7,11 +7,16 @@
 #include "UECodeCheckSampleGameInstance.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
-class UECODECHECKSAMPLE_API UUECodeCheckSampleGameInstance : public UGameInstance
-{
-	GENERATED_BODY()
-	
+class UECODECHECKSAMPLE_API UUECodeCheckSampleGameInstance
+    : public UGameInstance {
+  GENERATED_BODY()
+  
+protected:
+  TObjectPtr<UWorld> WorldWithoutUProp;
+  
+  UPROPERTY()
+  TObjectPtr<UWorld> WorldWithUProp;
 };
