aadd115642bab8c3062475b5f61ec916801d6f3f
Rafal Lazinski <rafal.lazinski@gmail.com>
2025-11-14 15:37:41 +0100

    feat: Add UUECodeCheckSampleGameInstance

---
+++ Source/UECodeCheckSample/UECodeCheckSampleGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "UECodeCheckSampleGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UECODECHECKSAMPLE_API UUECodeCheckSampleGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
